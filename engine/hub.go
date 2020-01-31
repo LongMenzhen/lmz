7f8818ac281b68a6cafd68c7ed3778abd5889b6b
cyrnicolase <nicolase.chen@gmail.com>
2020-01-31 15:52:30 +0800

    注éa文字修改

--- engine/hub.go
+++ engine/hub.go
@@ -9,32 +9,32 @@ var (
 	once sync.Once
 )
 
-// Hub 服务端房间仓库，管理所有的房间
-// 先创建好房间后，将房间加入到Hub 中
-// 然后就将创建好的房间ID 通知到各个客户端
-// 各个客户端在选择连接房间的时候，携带 group_id
+// Hub 服务端组仓库，管理所有的组
+// 先创建好组后，将组加入到Hub 中
+// 然后就将创建好的组ID 通知到各个客户端
+// 各个客户端在选择连接组的时候，携带 group_id
 // 仓库只能够初始化一次，所以要设计为单例
 type Hub struct {
-	Groups     map[int32]*Group // 仓库房间集合
-	Register   chan *Group      // 新建房间
-	Unregister chan *Group      // 销毁房间
+	Groups     map[int32]*Group // 仓库组集合
+	Register   chan *Group      // 新建组
+	Unregister chan *Group      // 销毁组
 }
 
-// AddGroup 将房间加入到仓库
+// AddGroup 将组加入到仓库
 func (h *Hub) AddGroup(group *Group) {
 	go group.Run()
 	h.Groups[group.ID] = group
 }
 
-// RemoveGroup 移除房间
+// RemoveGroup 移除组
 func (h *Hub) RemoveGroup(group *Group) {
 	if _, ok := h.Groups[group.ID]; ok {
-		group.Done <- true         // 关闭房间服务
-		delete(h.Groups, group.ID) // 从集合中删除该房间
+		group.Done <- true         // 关闭组服务
+		delete(h.Groups, group.ID) // 从集合中删除该组
 	}
 }
 
-// GroupByID 根据房间id查询仓库房间
+// GroupByID 根据组id查询仓库组
 func (h *Hub) GroupByID(groupID int32) *Group {
 	if group, ok := h.Groups[groupID]; ok {
 		return group
@@ -45,13 +45,13 @@ func (h *Hub) GroupByID(groupID int32) *Group {
 
 // Run 运行仓库监听
 func (h *Hub) Run() {
-	// 同步观察，后续是否有新的房间进来，如果有新房间创建或者结束
+	// 同步观察，后续是否有新的组进来，如果有新组创建或者结束
 	go func() {
 		for {
 			select {
-			case group := <-h.Register: // http 创建一个新房间
+			case group := <-h.Register: // http 创建一个新组
 				h.AddGroup(group)
-			case group := <-h.Unregister: // 某个操作结束后，销毁房间
+			case group := <-h.Unregister: // 某个操作结束后，销毁组
 				h.RemoveGroup(group)
 			}
 		}
